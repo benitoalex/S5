e2a2bc8a1750e4d46ce1c200b034014e0a51bdb7
Mac Pro <a@posteo.es>
2024-04-08 12:38:49 +0200

    Nivell 1 Exercici 3

--- onboarding-digital/projects/onboarding-digital/src/App.jsx
+++ onboarding-digital/projects/onboarding-digital/src/App.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import { Card } from './card.jsx';
+import timeManagementImage from './Images/time_managment.svg';
+import programmingImage from './Images/programming.svg';
+import meditationImage from './Images/meditation.svg';
 export function App() {
     const [currentStep, setCurrentStep] = useState(0);
 
     const tutorialData = [
         {
             title: 'Dedica moltes hores',
-            description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.'
+            description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
+            image: timeManagementImage,
+            bgColor: '#4a9b95'
         },{
             title: 'Programa projectes propis',
-            description: 'Més val 10 hores treballant en prjectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.'
+            description: 'Més val 10 hores treballant en prjectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
+            image: programmingImage,
+            bgColor: '#e0e0e0'
         }, {
             title: 'Procura descansar',
-            description: 'Descansar bé i desconectar són vitals. D`aquesta manera reduiras l`estrés i l`ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.'
+            description: 'Descansar bé i desconectar són vitals. D`aquesta manera reduiras l`estrés i l`ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.',
+            image: meditationImage,
+            bgColor: '#ffe061'
         }]
 
     const nextStep = () => {
--- onboarding-digital/projects/onboarding-digital/src/card.jsx
+++ onboarding-digital/projects/onboarding-digital/src/card.jsx
@@ -1,15 +1,29 @@
+import './card.css';
+
 export function Card({ step, nextStep }) {
+    const cardStyle = {
+        backgroundColor: step.bgColor
+    }
     return (
-        <article>
+        <article className='card-container'>
             <header>
-                <h1>{step.title}</h1>
-                <div>
-                    <div>
-                        {step.description}
+                <div className='container-fluid' style={cardStyle}>
+                    <div className='image-card'>
+                        <img src={step.image} alt={step.title} className='images img-fluid' />
                     </div>
-                    <button onClick={nextStep}>Next</button>
+                </div>
+                <div className='container'>
+                    <div className='text'>
+                        <h4>{step.title}</h4>
+                        <div>
+                            <div>
+                                {step.description}
+                            </div>
+                            <button className="arrow-button" onClick={nextStep}><span className="arrow"></span></button>
+                        </div>
+                    </div>
                 </div>
             </header>
         </article>
-    )
+    );
 }
